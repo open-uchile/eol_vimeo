9d16a640b6846cc0035a353c8898c3f564e57bb5
konialtamirano <cons.altamirano@gmail.com>
2021-07-12 17:16:17 -0400

    Test 10 add coverage badge

--- .github/test.sh
+++ .github/test.sh
@@ -14,7 +14,7 @@ cd ..
 
 cd /openedx/requirements/eol_vimeo
 pip install pytest-cov genbadge[coverage]
-sed -i '/--json-report/c addopts = --nomigrations --reuse-db --durations=20 --json-report --json-report-omit keywords streams collectors log traceback tests --json-report-file=none --cov=/openedx/requirements/eol_vimeo/eol_vimeo/ --cov-report term-missing --cov-report xml:/openedx/requirements/eol_vimeo/reports/coverage/coverage.xml --cov-fail-under 70 --cov-config=/openedx/requirements/eol_vimeo/.coveragerc' setup.cfg
+sed -i '/--json-report/c addopts = --nomigrations --reuse-db --durations=20 --json-report --json-report-omit keywords streams collectors log traceback tests --json-report-file=none --cov=/openedx/requirements/eol_vimeo/eol_vimeo/ --cov-report term-missing --cov-report xml:/openedx/requirements/eol_vimeo/reports/coverage/coverage.xml --cov-fail-under 70' setup.cfg
 
 cd /openedx/edx-platform
 EDXAPP_TEST_MONGO_HOST=mongodb python -Wd -m pytest --ds=cms.envs.test --junitxml=/openedx/edx-platform/reports/cms/nosetests.xml /openedx/requirements/eol_vimeo/eol_vimeo/tests.py
