d133d594ffe01e6852a6006254f4b79c3237112c
konialtamirano <cons.altamirano@gmail.com>
2021-07-13 10:50:49 -0400

    Test 21 add coverage badge

--- .github/commit.sh
+++ .github/commit.sh
@@ -3,6 +3,7 @@
 prepare_commit() {
   git config --global user.name "eolito"
   git config --global user.email 'eol-uchile@users.noreply.github.com'
+  git pull
   git add coverage-badge.svg
   git commit -m "Update coverage badge" 2> /dev/null
   if [ $? -eq 0 ]; then
